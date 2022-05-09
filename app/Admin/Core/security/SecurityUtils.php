e2c9b12b8e93d853f030ed01fc75c8f615a5fe56
dreamisfarfly <dreamisfarfly@gmail.com>
2022-05-09 16:11:01 +0800

    后台管理系统控制器

--- app/Admin/Core/security/SecurityUtils.php
+++ app/Admin/Core/security/SecurityUtils.php
@@ -11,6 +11,24 @@ class SecurityUtils
 {
 
     /**
+     * 获取用户账户
+     * @return mixed
+     */
+    public static function getUsername()
+    {
+        return self::getLoginUser()['sysUser']['userName'];
+    }
+
+    /**
+     * 获取用户
+     * @return mixed|null
+     */
+    public static function getLoginUser()
+    {
+        return (new TokenService())->getLoginUser();
+    }
+
+    /**
      * 是否为管理员
      *
      * @param int|null $userId 用户ID
